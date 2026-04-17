8c894625f7a0ebd1f1a79766c067d23503b93d90
guilhermebrunne@gmail.com <guilhermebrunne@gmail.com>
2026-04-16 22:15:35 -0300

    Primeiro commit

---
+++ UsuariosController.cs
@@ -0,0 +1,20 @@
+﻿using APIUsuarios.DAO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIUsuarios.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        [HttpGet]
+        [Route("listar")]
+        public IActionResult Listar()
+        {
+            var dao = new UsuarioDAO();
+            var usuarios = dao.ListarUsuarios();
+            return Ok(usuarios);
+        }
+    }
+}

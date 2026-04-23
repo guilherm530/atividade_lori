c8385b6477b7bc8b2445b6c1747c8fbb83091ecf
guilherm530 <guilhermebrunne@gmail.com>
2026-04-23 19:25:55 -0300

    DTO

---
+++ UsuarioDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace APIUsuarios.DTO
+{
+    public class UsuarioDTO
+    {
+        public int ID { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+    }
+}

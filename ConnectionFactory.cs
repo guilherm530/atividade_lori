a3deabe6dcb433b4bb8e84a80cc9e74dc6b74564
guilherm530 <guilhermebrunne@gmail.com>
2026-04-23 19:24:43 -0300

    Connection

---
+++ ConnectionFactory.cs
@@ -0,0 +1,13 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace APIUsuarios.DAO
+{
+    public class ConnectionFactory
+    {
+        public static MySqlConnection Build()
+        {
+            var connectionString = "Server=localhost;Database=Apostila;Uid=root;Pwd=root;";
+            return new MySqlConnection(connectionString);
+        }
+    }
+}

56a6287f857eb70d60cd15753a768c19c499829b
guilherm530 <guilhermebrunne@gmail.com>
2026-04-23 19:31:54 -0300

    Update UsuariosController.cs

--- UsuariosController.cs
+++ UsuariosController.cs
@@ -1,4 +1,5 @@
 ﻿using APIUsuarios.DAO;
+using APIUsuarios.DTO;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -16,5 +17,42 @@ namespace APIUsuarios.Controllers
             var usuarios = dao.ListarUsuarios();
             return Ok(usuarios);
         }
+
+        [HttpPost]
+        [Route("cadastrar")]
+        public IActionResult Cadastrar(UsuarioDTO usuario)
+        {
+            var dao = new UsuarioDAO();
+            dao.CadastrarUsuario(usuario);
+
+            return Ok("Usuário cadastrado com sucesso!");
+        }
+
+        [HttpPut]
+        [Route("atualizar/{id}")]
+        public IActionResult Atualizar([FromRoute] int id, [FromBody] UsuarioDTO usuario)
+        {
+            var dao = new UsuarioDAO();
+            var linhasAfetadas = dao.AtualizarUsuario(id, usuario);
+
+            if (linhasAfetadas == 0)
+                return NotFound("Usuário não encontrado.");
+
+            return Ok("Usuário atualizado com sucesso!");
+        }
+
+        [HttpDelete]
+        [Route("deletar/{id}")]
+        public IActionResult Deletar([FromRoute] int id)
+        {
+            var dao = new UsuarioDAO();
+            var linhasAfetadas = dao.DeletarUsuario(id);
+
+            if (linhasAfetadas == 0)
+                return NotFound("Usuário não encontrado.");
+
+            return Ok("Usuário excluído com sucesso!");
+        }
     }
 }
+
